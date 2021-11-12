5e18caec7cc09bb13e93ac960401442ff5517d7f
Niki Fereidooni <nikifereidooni@gmail.com>
2021-11-12 16:41:36 -0500

    started intiial draft of navbar, trying to build with css only

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./style.css";
 import { NavLink } from "react-router-dom";
 
 
 function Header(props) {
 
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
-    <nav class="main-nav">
-        <a href="index.html" class="logo">Simple Meditation Timer</a>
-        <ul class="navlinks">
-          <li>
+    <div className="header">
+      <div className="logo-nav">
+
+        <div className="logo-container">
+          <a href="#" className="logo">Simple Meditation Timer</a>
+        </div>
+
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
             <NavLink to="/">Home</NavLink>
           </li>
-          <li>
+          <li className="option" onClick={closeMobileMenu}>
             <NavLink to="/about">About</NavLink>
           </li>
         </ul>
-      </nav>
+
+      </div>
+
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <i class="fas fa-2x fa-times"></i>
+        ) : (
+          <i class="fas fa-2x fa-bars"></i>
+        )}
+      </div>
+
+    </div>
   );
 }
 
